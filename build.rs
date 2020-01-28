e758f3bdcbb977bc7937a17962f770b20176e944
Amédée d'Aboville <amedee.daboville@gmail.com>
2020-01-28 18:18:10 -0500

    Only whitelist k4a_* functions.

--- build.rs
+++ build.rs
@@ -17,6 +17,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_arg(format!("-I./vendor/include/{}", consts::OS))
+        .whitelist_function("k4a.*")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
